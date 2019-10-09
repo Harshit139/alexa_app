dea5164f43c2477329dbe5bc352a6aa657ce8e1c
Harshit <solankiharshit13@gmail.com>
2019-10-09 17:14:42 +0000

    Added comment for new print line

--- helloworld.java
+++ helloworld.java
@@ -3,6 +3,8 @@
 public class helloworld{
 	public static void main (String[] args){
 	System.out.println("Hello World");
+	
+	//prints, "welcome to git", in the terminal window.
 	System.out.println("Welcome to GIT");
 	}
 }
